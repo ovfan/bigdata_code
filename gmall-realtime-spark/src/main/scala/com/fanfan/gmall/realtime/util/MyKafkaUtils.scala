5365953767b338cfaad12b42dad2cb048e4d62d2
fanfan <fanfan88@88.com>
2022-06-07 15:35:45 +0800

    test

---
+++ gmall-realtime-spark/src/main/scala/com/fanfan/gmall/realtime/util/MyKafkaUtils.scala
@@ -0,0 +1,20 @@
+package com.fanfan.gmall.realtime.utilwang
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
+/**
+ * kafka工具类，用于消费和生产数据
+ *    使用sparkstreaming消费数据
+ */
+object MyKafkaUtils {
+  /**
+   * 从kafka中消费数据
+   * 获取到InputStream对象，
+   */
+
+
+
+  /**
+   * 往kafka中生产数据
+   */
+}
