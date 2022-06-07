9ba160a359e41fca453ba4f49b8eacd2803006bb
fanfan <fanfan88@88.com>
2022-06-08 00:57:54 +0800

    初始化sparkstreaming环境

--- spark/src/main/scala/com/fanfan/spark/sparkstreaming/SparkStreaming_Env01.scala
+++ spark/src/main/scala/com/fanfan/spark/sparkstreaming/SparkStreaming_Env01.scala
@@ -7,7 +7,7 @@ object SparkStreaming_Env01 {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("StreamingEnvTest")
     //创建sparkstreaming环境对象
-    // TODO SparkStreaming是基于spark的模块，底层也会采用soark的环境进行封装
+    // TODO SparkStreaming是基于spark的模块，底层也会采用soark的环境进行封装d
     //   第二个参数表示数据采集的周期，以毫秒为单位
     val ssc = new StreamingContext(conf, Duration(3000))
 
