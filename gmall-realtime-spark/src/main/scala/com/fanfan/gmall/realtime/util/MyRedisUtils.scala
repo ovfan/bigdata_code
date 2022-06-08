833bd38db02b91a94853bb09d4488bc3fd81794f
fanfan <1763058483@qq.com>
2022-06-08 13:56:08 +0800

    完成日志采集分流模块

--- gmall-realtime-spark/src/main/scala/com/fanfan/gmall/realtime/util/MyRedisUtils.scala
+++ gmall-realtime-spark/src/main/scala/com/fanfan/gmall/realtime/util/MyRedisUtils.scala
@@ -26,7 +26,7 @@ object MyRedisUtils {
       jedisPoolConfig.setMaxWaitMillis(5000) //忙碌时等待时长 毫秒
       jedisPoolConfig.setTestOnBorrow(true) //每次获得连接的进行测试
 
-      // 为redis设置密码 (需要设置 连接超时时间 + 密码)
+      // 为redis设置密码 (需要设置 连接超时时间 + 密码 )
       jedisPool = new JedisPool(jedisPoolConfig, host, port.toInt, 3000, passwd)
 
     }
